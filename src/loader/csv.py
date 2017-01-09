d9c733b42aaf47a5e316947ee492c097f658489f
Ergys Dona <errikosd@gmail.com>
2017-01-09 15:33:34 +0100

    loader: add load logic for csv loader
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- src/loader/csv.py
+++ src/loader/csv.py
@@ -2,7 +2,9 @@
 csv Loader module
 """
 
+import os
 import logging
+import csv
 from . import Loader
 
 _logger = logging.getLogger('loader.csv')
@@ -21,5 +23,25 @@ class CsvLoader(Loader):
         self.dir_in = dir_in
 
     def load(self, translator):
-        # TODO: Implement
-        pass
+        # Load system components
+        components_path = os.path.join(self.dir_in, 'components.csv')
+        with open(components_path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # skip header
+            for row in reader:
+                row[3] = int(row[3])  # convert quantity to int
+                translator.add_component(row)
+        # Load system logic
+        logic_path = os.path.join(self.dir_in, 'logic.csv')
+        with open(logic_path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # skip header
+            for row in reader:
+                translator.add_logic(row)
+        # Load system properties
+        properties_path = os.path.join(self.dir_in, 'properties.csv')
+        with open(properties_path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # skip header
+            for row in reader:
+                translator.add_property(row)
