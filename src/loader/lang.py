a5795bec84d9ec83c855229abac1fa04e597b869
Ergys Dona <errikosd@gmail.com>
2017-01-09 09:35:33 +0100

    loader: add lang loader stub
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

---
+++ src/loader/lang.py
@@ -0,0 +1,24 @@
+"""
+lang Loader module
+"""
+
+import logging
+from . import Loader
+
+_logger = logging.getLogger('loader.lang')
+
+
+def handler(args):
+    return LangLoader()
+
+
+class LangLoader(Loader):
+    """
+
+    """
+
+    def __init(self):
+        pass
+
+    def load(self, translator):
+        pass
