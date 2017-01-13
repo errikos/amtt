eba800905d22e54d57fbee737c8681c8eb20569d
Ergys Dona <errikosd@gmail.com>
2017-01-13 15:43:30 +0100

    loader: add XML loader stub
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

---
+++ src/loader/xml.py
@@ -0,0 +1,24 @@
+"""
+XML Loader module
+"""
+
+import logging
+from . import Loader
+
+_logger = logging.getLogger('loader.xml')
+
+
+def handler(args):
+    return XmlLoader()
+
+
+class XmlLoader(Loader):
+    """
+
+    """
+
+    def __init(self):
+        pass
+
+    def load(self, translator):
+        pass
