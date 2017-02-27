3d92654aaba0c31c85bdb95b984a3ea16d81fc38
Ergys Dona <errikosd@gmail.com>
2017-02-27 17:29:09 +0100

    rbd: major refactoring
    Introduce the notion of graphs for modelling the component
    and failure relations before constructing the RBD.
    
    A sophisticated algorithm that acts upon the two aforementioned
    graphs is to be implemented, in order to construct the output (RBD)
    graph.
    
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- src/exporter/isograph/rbd.py
+++ src/exporter/isograph/rbd.py
@@ -3,13 +3,12 @@ Module containing the classes needed to construct the reliability block diagram
 for Isograph Reliability Workbench exportation.
 """
 
+import os
 import logging
 import re
-from collections import deque
+import networkx as nx
 from collections import OrderedDict
-from sliding_window import window
-
-from .emitter.excel import RbdBlock, RbdNode, RbdConnection
+from copy import copy
 
 _logger = logging.getLogger(__name__)
 
@@ -29,10 +28,11 @@ class Component(object):
         self._children = []
 
     def __str__(self):
-        return 'rbd.Component: t:{},n:{},c:{},i:{},p:{},L:{}'.format(
-            self.type, self.name, self.code, self.instances, self.parent.name
-            if self.parent is not None else None, self.logic.name
-            if self.logic is not None else None)
+        # return 'rbd.Component: c:{},i:{},p:{},L:{}'.format(
+        #     self.name, self.instances,
+        #     self.parent.name if self.parent is not None else None,
+        #     self.logic.name if self.logic is not None else None)
+        return '{}: {}'.format(self.type, self.name)
 
     def __iter__(self):
         return iter(self._children)
@@ -75,6 +75,27 @@ class Component(object):
     @property
     def child_count(self):
         return len(self._children)
+
+
+class ComponentInstance(object):
+    def __init__(self, component, instance):
+        self._component = component
+        self._instance = instance
+
+    @property
+    def component(self):
+        """Returns the component object (metaclass) of the instance"""
+        return self._component
+
+    @property
+    def instance(self):
+        """Returns the instance number of the instance"""
+        return self._instance
+
+    @property
+    def full_name(self):
+        """Returns the fully qualified name of the instance"""
+        return '.'.join((self.component.name, str(self.instance)))
 
 
 class Logic(object):
@@ -126,161 +147,123 @@ class Rbd(object):
     """
 
     def __init__(self, flat_container):
-        self._flat_container = flat_container
-        self._component_index = OrderedDict()
-        self._failure_index = OrderedDict()
-        self._construct()
+        # Initialize components and failures index
+        self._components_index = OrderedDict()
+        self._failures_index = OrderedDict()
+        # Initialize graph structures
+        self._input_graph = nx.DiGraph()
+        self._components_graph = nx.DiGraph()
+        self._failures_graph = nx.DiGraph()
+        self._output_graph = nx.DiGraph()
+        # Construct the RBD
+        self._import(flat_container)
+        # DEBUG
+        # self.print_indexes()
 
-    def _construct(self):
-        _logger.info('Constructing in-memory RBD')
-        # Initialise the structures from the data given
-        self._initialize_structures()
-        # Detect the logic of the various components,
-        # for which a logic was not assigned explicitly.
-        self._detect_component_logic()
-        _logger.info('Finished constructing in-memory RBD')
+    def _import(self, flat_container):
+        _logger.info("Importing model definition in memory")
+        self._build_lookup_indexes(flat_container)
+        self._build_graphs()
 
-    def _initialize_structures(self):
-        # Create ROOT Component and add to index
+    def _build_lookup_indexes(self, flat_container):
+        def select_index(fcomp):
+            t = fcomp.type.lower()
+            is_structural = t in ('basic', 'compound', 'group')
+            is_failure = t in ('failurenode', 'failureevent')
+            return self._components_index if is_structural \
+                else self._failures_index if is_failure else None
+
+        _logger.info("Building indexes")
+        # Create a component for the Root node
         root = Component('Compound', 'ROOT', None, 1, Logic('ROOT'))
-        self._component_index[root.name] = root
-        # Build components and failures index
-        for f_component in self._flat_container.component_list:
-            index = self._component_index if f_component.type.lower() in (
-                'compound', 'basic', 'group') else self._failure_index
-            index[f_component.name] = Component(
-                f_component.type, f_component.name, f_component.code,
-                f_component.instances)
-        # Assign parents
-        for f_component in self._flat_container.component_list:
-            index = self._component_index if f_component.type.lower() in (
-                'compound', 'basic', 'group') else self._failure_index
-            parent_index = self._component_index \
-                if f_component.parent in self._component_index \
-                else self._failure_index \
-                if f_component.parent in self._failure_index \
+        self._components_index[root.name] = root
+        # Add the rest of the components to its respective index
+        for fcomp in flat_container.component_list:
+            index = select_index(fcomp)
+            if index is None:
+                _logger.warning(
+                    "Component {} has invalid Type".format(fcomp.name))
+            else:
+                index[fcomp.name] = Component(
+                    type=fcomp.type,
+                    name=fcomp.name,
+                    code=fcomp.code,
+                    instances=fcomp.instances)
+        # Assign the component parents
+        for fcomp in flat_container.component_list:
+            index = select_index(fcomp)
+            parent_index = self._components_index \
+                if fcomp.parent in self._components_index \
+                else self._failures_index \
+                if fcomp.parent in self._failures_index \
                 else None
             if parent_index is None:
-                _logger.warning('Component "{}" has an invalid parent'.format(
-                    f_component.name))
+                _logger.warning(
+                    'Component "{}" has an invalid parent'.format(fcomp.name))
             else:
-                index[f_component.name].parent = parent_index[
-                    f_component.parent]
+                # if parent_index != index:
+                #     p = copy(parent_index[fcomp.parent])
+                #     p.parent = None
+                #     index[p.name] = p
+                index[fcomp.name].parent = parent_index[fcomp.parent]
         # Assign logic
-        for f_logic in self._flat_container.logic_list:
-            index = self._component_index if f_logic.type.lower(
-            ) == 'inherited' else self._failure_index
+        for f_logic in flat_container.logic_list:
+            index = self._components_index if f_logic.type.lower(
+            ) == 'inherited' else self._failures_index
             index[f_logic.component].logic = Logic(f_logic.logic)
-        # Assign children
-        for name, component in self._component_index.items():
-            parent = component.parent
-            if parent is not None and component.type.lower() in (
-                    'compound', 'basic', 'group'):
-                self._component_index[parent.name].add_child(component)
 
-    def _detect_component_logic(self):
-        """
-        For the components that have no logic assigned, but have children,
-        tries to figure out their logic based on the FaultNodes/FaultEvents
-        in the definition.
-        """
-        for component in self._component_index.values():
-            if component.logic is None and component.child_count > 0:
-                pass
+    def _build_graphs(self):
+        _logger.info("Building graphs")
+        # Build the components graph
+        self._components_graph.add_nodes_from(self._components_index.values())
+        [   # Add edges
+            self._components_graph.add_edge(comp.parent, comp)
+            for comp in filter(lambda c: c.parent is not None,
+                               self._components_index.values())
+        ]
+        # Build the failures graph
+        self._failures_graph.add_nodes_from(self._failures_index.values())
+        [
+            self._failures_graph.add_edge(fail.parent, fail)
+            for fail in filter(lambda f: f.parent is not None,
+                               self._failures_index.values())
+        ]
+        self.export_graphs()
 
-    def serialize(self, emitter):
-        """
-        Serialise the components to the emitter.
-        """
-        _logger.info('Serialising RBD')
-        # The components remaining to be processed. Contents shall be tuples of
-        # type <Component, deque>, where deque is the page prefix of the comp.
-        component_stack = deque()
-        root_component = self._component_index['ROOT']
-        component_stack.appendleft(
-            Rbd.PreSerializedComponent(root_component, deque()))
-        while component_stack:
-            current = component_stack.popleft()
-            self._serialize_component(current, emitter)
-            for child in current.component:
-                [   # If more than one instances of a child exist within
-                    # another component, then append an ID to the child's
-                    # path. Otherwise, just use the path of the parent.
-                    component_stack.appendleft(
-                        Rbd.PreSerializedComponent(child, current.path +
-                                                   deque([i])))
-                    for i in range(child.instances, 0, -1)
-                ] if child.instances > 1 else component_stack.appendleft(
-                    Rbd.PreSerializedComponent(child, current.path))
-
-        _logger.info('Finished serialising RBD')
-
-    def _serialize_component(self, packed_component, emitter):
-        """
-        Serialises the given PreSerializedComponent object
-        to Isograph-specific tuples.
-        """
-
-        def make_path(p):
-            return '.' + '.'.join([str(t) for t in p]) \
-                if len(p) > 1 else '.' + str(p[0]) if len(p) == 1 else ''
-
-        prefix = make_path(packed_component.path)
-        component = packed_component.component
-
-        # if component has no children, then nothing to do, return
-        if component.child_count == 0:
-            return
-
-        if component.logic is None:
-            component.logic = Logic('AND')
-
-        # serialise the ROOT component and return
-        if component.logic.name == 'ROOT':
-            for child in component:
-                emitter.add_block(
-                    RbdBlock(
-                        Id=child.name, Page=None, XPosition=0, YPosition=0))
-            return
-
-        logic = component.logic
-
-        # set some layout specific variables
-        xs, ys = 250, 60
-        dx, dy = 0, 0
-
-        if logic.name == 'AND':
-            dx, dy = 150, 0
-        elif logic.name in ('OR', 'ACTIVE'):
-            dx, dy = 0, 150
-        if logic.name == 'ACTIVE':
-            xs += 150
-
-        for block in self._layout_basic_blocks(component, prefix, xs, ys, dx,
-                                               dy):
-            emitter.add_block(block)
-
-        for connection in self._layout_basic_connections():
-            # emitter.add_connection(connection)
-            pass
+    def export_graphs(self):
+        """Exports the graphs to PNG files under the ./graphs/ directory"""
+        output_dir = os.path.join(os.path.abspath(os.path.curdir), 'graphs')
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir, mode=0o755)
+        p = nx.drawing.nx_pydot.to_pydot(self._components_graph)
+        p.write_png(os.path.join(output_dir, 'components.png'))
+        p = nx.drawing.nx_pydot.to_pydot(self._failures_graph)
+        p.write_png(os.path.join(output_dir, 'failures.png'))
 
     @staticmethod
-    def _layout_basic_blocks(component, prefix, xs, ys, dx, dy):
-        k = 0
-        for child in component:
-            id_spec = '{}{}.{}' if child.instances > 1 else '{}{}'
-            for i in range(child.instances):
-                yield RbdBlock(
-                    Id=id_spec.format(child.name, prefix, i + 1),
-                    Page='{}{}'.format(child.parent.name, prefix),
-                    XPosition=xs + k * dx,
-                    YPosition=ys + k * dy)
-                k += 1
+    def _layout_graph(graph):
+        ts_list = nx.topological_sort(graph)
+        L = nx.bfs_successors(graph, ts_list[0])
+        pos = {ts_list[0]: (0, 0)}
+        dx, dy = 5, 3
+        x, y = dx, 0
+        for node in ts_list:
+            if node in L:
+                for v in L[node]:
+                    pos[v] = (x, y)
+                    y += dy
+                x += dx
+                y = 0
+        return pos
 
-    def _layout_basic_connections(self):
-        yield
+    def print_indexes(self):
+        """DEBUG"""
+        print("=== Components")
+        for comp in self._components_index.values():
+            print(comp)
+        print("===")
 
-    class PreSerializedComponent(object):
-        def __init__(self, component, path):
-            self.component = component
-            self.path = path
+        print("=== Failures")
+        for comp in self._failures_index.values():
+            print(comp)
+        print("===")
