bb4340e47d0b35ab7a2bb273b0cf91e7fd9e3a41
Ergys Dona <errikosd@gmail.com>
2017-04-25 15:26:55 +0200

    ir: support reusable components
    Change the IR logic to support reusable components.
    Change reflected in constructed components graph.
    TODO: Associate objects used by the exporter.rbd module.
    
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- amtt/translator/ir.py
+++ amtt/translator/ir.py
@@ -6,10 +6,9 @@ Can be considered as the Intermediate Representation (IR) of the translator.
 import os
 import logging
 import networkx as nx
-import itertools
 from collections import OrderedDict
 
-from .entities import SystemElement, ElementLogic
+from amtt.errors import TranslatorError
 
 _logger = logging.getLogger(__name__)
 
@@ -28,6 +27,8 @@ class IRContainer(object):
         self._components_index = OrderedDict()
         self._failures_index = OrderedDict()
         # Initialize graph structures
+        self._raw_input_graph = nx.DiGraph(
+            filename='raw_input.png', **IR_GRAPH_ATTRIBUTES)
         self._components_graph = nx.DiGraph(
             filename='components.png', **IR_GRAPH_ATTRIBUTES)
         self._failures_graph = nx.DiGraph(
@@ -36,78 +37,84 @@ class IRContainer(object):
     def load_from_rows(self, row_container):
         """Loads the model from the provided row container"""
         _logger.info('Importing model from rows')
-        self._build_lookup_indexes(row_container)
-        self._build_graphs()
+        self._build_graphs(row_container)
         self._loaded = True
 
-    def _build_lookup_indexes(self, row_container):
-        def select_index(fcomp):
-            t = fcomp.type.lower()
-            is_structural = t in ('basic', 'compound', 'group')
-            is_failure = t in ('failurenode', 'failureevent')
-            return self._components_index if is_structural \
-                else self._failures_index if is_failure else None
-
-        _logger.info('Building indexes')
-        # Create a component for the Root element
-        root = SystemElement('Compound', 'ROOT', 'ROOT', 1)
-        root.logic = ElementLogic('ROOT')
-        self._components_index[root.name] = root
-        # Add the rest of the components to their respective index
-        for crow in row_container.component_list:
-            index = select_index(crow)
-            if index is None:
-                _logger.warning(
-                    'Component {} has invalid Type'.format(crow.name))
-            else:
-                index[crow.name] = SystemElement(
-                    type=crow.type,
-                    name=crow.name,
-                    code=crow.code,
-                    instances=crow.instances)
-        # Assign the element parents
-        for crow in row_container.component_list:
-            index = select_index(crow)
-            parent_index = self._components_index \
-                if crow.parent in self._components_index \
-                else self._failures_index \
-                if crow.parent in self._failures_index \
-                else None
-            if parent_index is None:
-                _logger.warning(
-                    'Component "{}" has an invalid parent'.format(crow.name))
-            else:
-                index[crow.name].parent = parent_index[crow.parent]
-        # Assign logic
-        for lrow in row_container.logic_list:
-            index = self._components_index \
-                if lrow.type.lower() == 'inherited' \
-                else self._failures_index
-            index[lrow.component].logic = ElementLogic(lrow.logic)
-
-    def _build_graphs(self):
+    def _build_graphs(self, row_container):
         _logger.info('Building graphs')
-        # Build the components graph
-        [   # Add edges
-            self._components_graph.add_edge(comp.parent.id, comp.id)
-            for comp in filter(lambda c: c.parent is not None,
-                               self._components_index.values())
+        # Build raw input graph
+        [
+            self._raw_input_graph.add_edge(row.parent, row.name)
+            for row in row_container.component_list if row.parent != '*'
         ]
-        # Associate objects
-        for comp in filter(lambda c: c.id in self._components_graph,
-                           self._components_index.values()):
-            self._components_graph.node[comp.id].update(obj=comp)
-        # Build the failures graph
-        [   # Add edges
-            self._failures_graph.add_edge(fail.parent.id, fail.id)
-            for fail in filter(lambda f: f.parent is not None,
-                               self._failures_index.values())
-        ]
-        # Associate objects
-        for fail in filter(lambda f: f.id in self._failures_graph,
-                           itertools.chain(self._failures_index.values(),
-                                           self._components_index.values())):
-            self._failures_graph.node[fail.id].update(obj=fail)
+        # Check if raw input graph contains cycles
+        if not nx.is_directed_acyclic_graph(self._raw_input_graph):
+            _logger.error('Input model contains a component cycle')
+            raise TranslatorError('Input model contains a component cycle')
+        # Build components graph from raw input graph
+        self._build_components_graph()
+
+    def _build_components_graph(self):
+        """
+        Given the raw input graph, builds the components graph.
+        """
+
+        def relabel(label, node):
+            """
+            Relabels label to include the base name of node as the last token
+            of its prefix.
+            
+            The label and node tokens are considered to be separated by dots.
+            
+            The node base name is its last token.
+            
+            If label contains n tokens, the prefix is considered to be the
+            first n-1 tokens. Thus, s is appended to the prefix and the new
+            label contains n+1 tokens.
+            """
+            node_tokens = node.split('.')
+            tokens = label.split('.')
+            tokens.insert(-1, str(node_tokens[-1]))
+            return '.'.join(tokens)
+
+        # -- create temporary graph, to be the components graph
+        g = self._components_graph
+        # -- copy the raw input graph to tmp1
+        g.add_edges_from(self._raw_input_graph.edges_iter())
+        changed = True
+        # -- Fixed-Point algorithm:
+        # -- Iterate until tmp1 and tmp2 remain identical after last iteration
+        while changed:
+            changed = False
+            for u, v in nx.bfs_edges(g, 'ROOT'):
+                if g.in_degree(v) > 1:
+                    # Find the predecessors of the current node
+                    predecessors = [
+                        x for x in nx.all_neighbors(g, v)
+                        if (x, v) in g.edges()
+                    ]
+                    # Disconnect predecessors from v
+                    g.remove_edges_from((x, v) for x in predecessors)
+                    # Clone the sub-graph which has v as its root
+                    sub_graph = g.subgraph(
+                        nbunch=[v] + list(nx.dfs_preorder_nodes(g, v)))
+                    # Remove the sub-graph edges from the graph
+                    g.remove_nodes_from(sub_graph.nodes_iter())
+                    # For each predecessor, do the following:
+                    for p in predecessors:
+                        # -- obtain a copy of the sub-graph with renamed nodes
+                        # -- by preceding the predecessor name.
+                        rsub = nx.relabel_nodes(sub_graph, {
+                            o: relabel(o, p)
+                            for o in sub_graph.nodes_iter()
+                        })
+                        # -- add and connect the relabeled sub-graph (rsub)
+                        # -- to the current predecessor (p)
+                        g.add_edge(p, relabel(v, p))
+                        g.add_edges_from(rsub.edges_iter())
+                    changed = True
+                    break
+        # end of method
 
     def export_graphs(self, output_dir):
         """
@@ -118,7 +125,8 @@ class IRContainer(object):
         output_dir = os.path.join(os.path.abspath(output_dir), 'graphs')
         if not os.path.isdir(output_dir):
             os.makedirs(output_dir, mode=0o755)
-        for g in (self._components_graph, self._failures_graph):
+        for g in (self._raw_input_graph, self._components_graph,
+                  self._failures_graph):
             pdg = nx.drawing.nx_pydot.to_pydot(g)
             pdg.write_png(os.path.join(output_dir, g.graph['filename']))
 
