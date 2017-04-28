d8c2eeb162e178feabcc49afb77596877aa270b6
Ergys Dona <errikosd@gmail.com>
2017-04-28 15:16:53 +0200

    loader.csv: minor refactoring
    Refactor amtt.loader.csv to reflect changes in IR
    
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- amtt/loader/csv.py
+++ amtt/loader/csv.py
@@ -22,29 +22,15 @@ class CsvLoader(Loader):
     def __init__(self, dir_in):
         self.dir_in = dir_in
 
-    def load(self, container):
-        # csv file names are listed here...
-        file_names = [
-            'components.csv',
-            'logic.csv',
-        ]
-        # ... and the flat handler methods here.
-        method_names = [
-            'add_component',
-            'add_logic',
-        ]
-        # Attention: file_names and method_names above are expected to have
-        #            a 1-1 correspondence
-        for file_name, method_name in zip(file_names, method_names):
-            with open(os.path.join(self.dir_in, file_name)) as f:
+    def load_new(self, container):
+        # For each sheet is sheet definitions (here: file)
+        for sheet_type, sheet_name in self.sheet_definitions_iter():
+            filename = sheet_name + '.csv'
+            with open(os.path.join(self.dir_in, filename)) as f:
                 reader = csv.DictReader(f)
                 # convert csv field (column) names to lowercase
                 reader.fieldnames = [
                     field.lower().replace(' ', '_')
                     for field in reader.fieldnames
                 ]
-                # add the flat components to the translator structures
-                [
-                    getattr(container, method_name)(**row) for row in reader
-                    if row
-                ]
+                [container.add_row(sheet_type, **row) for row in reader if row]
