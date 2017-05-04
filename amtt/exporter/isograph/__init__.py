4d9a7b5d6cd131f30adf48e431f515579d84cc19
Ergys Dona <errikosd@gmail.com>
2017-05-04 12:12:42 +0200

    isograph: add name normalization method to exporter
    If the IR container determines that the model uses
    template components, rename them to avoid having really
    long names (Isograph limit: 40 characters).
    
    Renaming changes a component's name to a number (ID)
    and sets the base block name as the description.
    
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- amtt/exporter/isograph/__init__.py
+++ amtt/exporter/isograph/__init__.py
@@ -2,7 +2,10 @@
 Exporter module for Isograph Availability Workbench.
 """
 import logging
+import networkx as nx
+from itertools import count
 
+from amtt.translator.ir import component_basename
 from amtt.exporter import Exporter
 from amtt.exporter.isograph.emitter.xml import XmlEmitter
 from amtt.exporter.isograph.rbd import Rbd
@@ -20,7 +23,23 @@ class IsographExporter(Exporter):
         self._translator = translator
         self._emitter = XmlEmitter(translator.output_basedir)
 
+    @staticmethod
+    def normalize_block_names(ir_container):
+        g = ir_container.component_graph
+        if ir_container.uses_templates:
+            relabel_mapping = {n: c for n, c in zip(g.nodes_iter(), count(1))}
+            del relabel_mapping['ROOT']  # We don't want to relabel ROOT
+            nx.relabel_nodes(g, relabel_mapping, copy=False)
+            for u, v in nx.bfs_edges(g, 'ROOT'):
+                vo = g.node[v]['obj']  # Get a hold of the associated object
+                # Set base name as description
+                vo.description = component_basename(vo.name)
+                # Set ID number as name
+                vo.name = v
+
     def export(self):
+        # Normalize block names, if necessary
+        self.normalize_block_names(self._translator.ir_container)
         # Create block diagram from input
         rbd = Rbd()
         rbd.from_ir_container(self._translator.ir_container)
