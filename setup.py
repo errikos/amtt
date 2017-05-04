5e1f92625ff68e85489069412f8ea6304250fb11
Ergys Dona <errikosd@gmail.com>
2017-05-04 16:40:49 +0200

    setup: fix version and status
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='0.3.0',
     description='Availability Modelling Translation Toolkit',
     long_description=long_description,
 
@@ -84,7 +84,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Manufacturing',
